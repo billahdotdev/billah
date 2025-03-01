e6ff738ae1c9f6b1c1ebef12a22243c58c75fbf1
Masum Billah <emasumbillah@gmail.com>
2025-03-01 08:36:43 +0600

    hange content of Component

--- src/components/Component.jsx
+++ src/components/Component.jsx
@@ -2,7 +2,7 @@ import '../styles/Component.css';
 const Component = ({ children }) => {
   return (
       <>
-      <div className="hello"><h1>Hello!</h1></div>
+      <div className="hello"><h1>Hello!!!</h1></div>
       
       </>
   );
