053014d1b81fcbb35859e54f9eaff36f0a1cd10a
Masum Billah <emasumbillah@gmail.com>
2025-03-01 10:14:03 +0600

    Add New Component

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import LayoutWrapper from './components/LayoutWrapper';
 import PenWrapper from './components/PenWrapper';
 import Component from './components/Component';
+import Neo from './components/Neo';
 
 const App = () => {
     return (
         <LayoutWrapper>
             <PenWrapper>
                 <Component />
+                <Neo />
             </PenWrapper>
         </LayoutWrapper>
     );
