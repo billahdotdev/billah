482f0613d30a586de22e10a27341d50cf6384063
Masum Billah <emasumbillah@gmail.com>
2025-03-27 00:38:29 +0600

    Change image location

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -45,7 +45,7 @@ const About = () => {
             variants={fadeInUp}
           >
             <div className="about-image-container">
-              <img src="../src/assets/MasumB.JPG" alt="Portrait" className="about-image" />
+              <img src="https://avatars.githubusercontent.com/u/112099343?s=400&u=0090532ddc022edb81897e8c287cd91dbbf0a876&v=4" alt="Portrait" className="about-image" />
               <motion.div
                 className="image-overlay"
                 initial={{ opacity: 0 }}
