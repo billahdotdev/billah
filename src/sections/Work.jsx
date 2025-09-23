24540dd714b6a6ffa500fef0b1d5b295933147f9
billahdotdev <billahdotdev@gmail.com>
2025-09-23 12:02:14 +0600

    Improving products

--- src/sections/Work.jsx
+++ src/sections/Work.jsx
@@ -57,7 +57,13 @@ const Work = () => {
         'https://images.unsplash.com/photo-1507238691740-187a5b1d37b8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1455&q=80',
       description:
         'Professional portfolio website to showcase your work with elegant design and smooth animations.',
-      technologies: ['React', 'Pure CSS', 'JavaScript', 'Framer Motion'],
+      technologies: [
+        'Vite',
+        'React',
+        'Pure CSS',
+        'JavaScript',
+        'Framer Motion',
+      ],
       features: [
         'Custom responsive design',
         'Project showcase gallery',
